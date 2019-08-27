e5570166fa44f81453e2b7daadb829613b296fd8
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-27 22:18:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,10 @@
 class Song 
-  attr_accessor :song 
+  attr_accessor :song, :artist
   
   def initialize(song)
     @song = song 
+    @artist - artist 
+    
+    artist.song << self 
   end
 end
